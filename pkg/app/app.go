b80d8625bbe527babb2f32c419143162284087de
ageekymonk <ramzthecoder@gmail.com>
2020-04-20 08:28:34 +1000

    Basic display of public tweets

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,6 +1,12 @@
 package app
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/techniumlabs/gotwit/pkg/config"
@@ -8,7 +14,7 @@ import (
 
 type App struct {
 	Config *config.Config
-	client *twitter.Client
+	Client *twitter.Client
 }
 
 func NewApp(cfgFile string) (*App, error) {
@@ -22,5 +28,35 @@ func NewApp(cfgFile string) (*App, error) {
 	httpClient := oconfig.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 
-	return &App{Config: c, client: client}, nil
+	return &App{Config: c, Client: client}, nil
+}
+
+func (a *App) UserTweets(c chan *twitter.Tweet) {
+
+	// params := &twitter.StreamUserParams{
+	//	With:          "followings",
+	//	StallWarnings: twitter.Bool(true),
+	// }
+	// stream, err := a.App.Client.Streams.User(params)
+	params := &twitter.StreamSampleParams{
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := a.Client.Streams.Sample(params)
+	if err != nil {
+		log.Errorf("%s", err.Error())
+		return
+	}
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		c <- tweet
+	}
+	go demux.HandleChan(stream.Messages)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	stream.Stop()
 }
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -1,25 +1,59 @@
 package ui
 
 import (
+	"fmt"
+	"io"
+
+	"github.com/dghubble/go-twitter/twitter"
 	"github.com/rivo/tview"
+	log "github.com/sirupsen/logrus"
 	gotwitApp "github.com/techniumlabs/gotwit/pkg/app"
 )
 
 type AppUI struct {
 	UIApp *tview.Application
+	App   *gotwitApp.App
 }
 
 func NewUI(app *gotwitApp.App) *AppUI {
 
 	uiapp := tview.NewApplication()
 
-	return &AppUI{UIApp: uiapp}
+	return &AppUI{UIApp: uiapp,
+		App: app}
 
 }
 
 func (a *AppUI) Render() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := a.UIApp.SetRoot(box, true).Run(); err != nil {
+	// box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+
+	if err := a.UIApp.SetRoot(a.GetTimelineView(), true).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func (a *AppUI) GetTimelineView() *tview.TextView {
+	log.Println("Setting Timeline View")
+	textView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetChangedFunc(func() {
+			a.UIApp.Draw()
+		})
+
+	go func() {
+		c := make(chan *twitter.Tweet)
+		go a.App.UserTweets(c)
+		for t := range c {
+			DisplayTweet(textView, t)
+		}
+	}()
+
+	textView.SetBorder(true)
+	return textView
+}
+
+func DisplayTweet(w io.Writer, t *twitter.Tweet) {
+	fmt.Fprintf(w, "[red]%s[white]\n", t.User.Name)
+	fmt.Fprintf(w, "%s\n\n", t.Text)
 }
